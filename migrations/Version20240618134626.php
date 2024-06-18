1ada0555880cc1d488ca36ba06ea69e759f457f0
William-lh <lherm.william@gmail.com>
2024-06-18 16:08:24 +0200

    correctif security contact

--- migrations/Version20240618134626.php
+++ migrations/Version20240618134626.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240201092317 extends AbstractMigration
+final class Version20240618134626 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20240201092317 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE projet CHANGE description description VARCHAR(1000) NOT NULL');
+        $this->addSql('ALTER TABLE contact CHANGE name name VARCHAR(20) NOT NULL, CHANGE email email VARCHAR(50) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE projet CHANGE description description TEXT NOT NULL');
+        $this->addSql('ALTER TABLE contact CHANGE name name VARCHAR(255) NOT NULL, CHANGE email email VARCHAR(255) NOT NULL');
     }
 }
